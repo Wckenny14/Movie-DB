93075201383039d04f16b90808fc3920cfaf7423
Wayne Kenny <wayne.kenny03@gmail.com>
2023-04-09 17:21:47 +1000

    first ommit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import { useState, useEffect } from 'react'
+import SearchIcon from './search.svg'
+import MovieList from './MovieList';
+
+const API_URL = 'http://www.omdbapi.com?apikey=cadce31a'
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  
+  useEffect(() => {
+    searchMovies('Batman');
+  }, []);
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search)
+  }
+
+  return (
+    <div className='app'>
+      <h1>Movies DB</h1>
+
+      <div className='search'>
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder='Search for movies'
+        />
+        <img 
+          src={SearchIcon}
+          alt='search'
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? (
+        <div className='container'>
+          {movies.map((movie) => (
+            <MovieList movie={movie} />
+          ))}
+        </div>
+      ) : (
+        <div className='empty'>
+          <h2>No Movies Found</h2>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
